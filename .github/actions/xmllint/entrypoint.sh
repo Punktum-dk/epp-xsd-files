64d27cfbc1dedf191514ec8b482623a6ff2e3f00
jonasbn <jonasbn@dk-hostmaster.dk>
2021-10-31 12:55:47 +0100

    Attempting with eval

--- .github/actions/xmllint/entrypoint.sh
+++ .github/actions/xmllint/entrypoint.sh
@@ -8,9 +8,12 @@ echo "--------------------------------------"
 
 shopt -s globstar
 
+command="xmllint $1 --schema $2 --noout"
+
 #xmllint --noout --schema epp.xsd xml/{,**/}*.xml
 # shellcheck disable=SC2086
-xmllint $1 --schema $2 --noout
+#xmllint $1 --schema $2 --noout
+eval $command
 
 EXITCODE=$?
 
